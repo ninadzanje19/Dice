49545f4d2c4ae55bc38217109953eef66b9b2fc6
ninadzanje19 <73586946+ninadzanje19@users.noreply.github.com>
2021-07-26 12:55:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var dice1 = Math.random();
+dice1 = Math.floor(dice1 * 6) + 1;
+var dice1Image = "dice" + dice1 + ".png";
+var dice1ImageSrc = "images/" + dice1Image;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", dice1ImageSrc);
+
+var dice2 = Math.random();
+dice2 = Math.floor(dice2 * 6) + 1;
+var dice2Image = "dice" + dice2 + ".png";
+var dice2ImageSrc = "images/" + dice2Image;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", dice2ImageSrc);
+
+if(dice1 > dice2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!!!";
+}
+else if(dice1 < dice2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!!!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Its a Draw :(";
+
+}
